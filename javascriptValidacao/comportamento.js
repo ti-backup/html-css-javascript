f3030544ed6cf4677962a3e73f7510dfba13987d
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-12-05 21:56:03 -0300

    Finalização do script de validação de formulário

--- javascriptValidacao/comportamento.js
+++ javascriptValidacao/comportamento.js
@@ -29,18 +29,40 @@ function visualizarAreas(){
 }
 
 function confirmarSenha(){
-    var senha = document.getElementById("senha").value;
     var confirma = document.getElementById("confirma").value;
 
-    if (senha != confirma)
-    {
-        var msg = "As senhas não conferem.";
+    if (confirma.length > 0){
+        var senha = document.getElementById("senha").value;
+        if (senha != confirma)
+        {
+            var msg = "As senhas não conferem.";
+            alert(msg);
+            document.getElementById("erroSenha").innerHTML = msg;
+        }
+        else {
+            document.getElementById("erroSenha").innerHTML = "";
+        }
+    }
+}
+
+function verificaSenha(){
+    var senha = document.getElementById("senha").value;
+    var msg = "";
+
+    if(senha.length != 8)
+        msg += "- A senha deve ter 8 caracteres\n";
+
+    if(!senha.match(/[a-z]+/))
+        msg += "- A senha deve ter pelo menos 1 letra minuscula.\n";
+
+    if(!senha.match(/[A-Z]+/))
+        msg += "- A senha deve ter pelo menos 1 letra maiuscula.\n";
+
+    if(!senha.match(/[0-9]+/))
+        msg += "- A senha deve ter pelo menos 1 número.\n";
+
+    if (msg != "")
         alert(msg);
-        document.getElementById("erroSenha").innerHTML = msg;
-    }
-    else {
-        document.getElementById("erroSenha").innerHTML = "";
-    }
 }
 
 function validacao(){
@@ -59,6 +81,27 @@ function validacao(){
         msg += "- O campo idade deve ser preenchimento com um valor entre 1 e 125 \r\n";
     }
 
+    /* VERIFICA AREAS DE INTERESSE */
+    var areas = document.getElementsByName("areas[]");
+    var i;
+    var contador = 0;
+
+    for(i = 0; i < areas.length; i++){
+        if (areas[i].checked){
+            contador++;
+        }
+    }
+
+    if (contador < 1)
+        msg += "- Selecione pelo menos uma área de interesse\r\n";
+
+    /* VERIFICA CEP */
+    var cep = document.getElementById("cep").value;
+    var padraoCep = /^[0-9]{5}-?[0-9]{3}$/;
+
+    if (!padraoCep.test(cep))
+        msg += "- O campo CEP deve ser preenchido com um formato válido\r\n";
+    
     if (msg != "")
         alert("Verifique os seguinte campos: \r\n" + msg);
     else autorizacao = true;
